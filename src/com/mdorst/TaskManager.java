6d65eecf9cd5cbddec72cc3bcc19be04057c1b12
Michael Dorst <code@mikefdorst.com>
2016-01-22 20:54:12 -0800

    Add TaskManager.input()

--- src/com/mdorst/TaskManager.java
+++ src/com/mdorst/TaskManager.java
@@ -1,15 +1,34 @@
 package com.mdorst;
 
+import com.mdorst.container.HashTable;
+
 /**
  * Michael Dorst
  * CISP 430
  * Assignment 1 - Hashing
  */
 public class TaskManager {
+
+    HashTable<String, String> hashTable;
+
+    /**
+     * Input from datain.txt
+     */
+    public void input() {
+        Data datain = new Data("data/datain.txt");
+        String line;
+        while ((line = datain.getLine()) != null) {
+            hashTable.add(line.substring(0, 10), line.substring(10));
+        }
+    }
     // input from datain.txt
     // output to dataout.txt
     // restore from dataout.txt
     // display contents
     // search
     // compute hash efficiency statistics
+
+    public TaskManager() {
+        hashTable = new HashTable<>(new StringHash());
+    }
 }
