370afa51bfafc472cd7099fe64adc67f8a326e79
Michael Dorst <code@mikefdorst.com>
2016-01-22 20:19:51 -0800

    Add restore capabilities

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -9,17 +9,30 @@ import java.io.UnsupportedEncodingException;
 public class Main {
 
     public static void main(String[] args) {
-        Data data = new Data("data/datain.txt");
+        Data datain = new Data("data/datain.txt");
+        Data dataout = new Data("data/dataout.txt");
         HashTable<String, String, StringHash> hashTable = new HashTable<>(new StringHash());
         String line;
-        while ((line = data.getLine()) != null) {
-            hashTable.add(line.substring(0, 9), line.substring(10));
+
+        while ((line = datain.getLine()) != null) {
+            hashTable.add(line.substring(0, 10), line.substring(10));
         }
         try {
             hashTable.print(new PrintStream("data/dataout.txt", "UTF-8"));
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        hashTable.print(System.out);
+
+        hashTable = new HashTable<>(new StringHash());
+        while ((line = dataout.getLine()) != null) {
+            if (line.startsWith(" ")) {
+                hashTable.add(line.substring(11, 21), line.substring(24));
+            }
+        }
+        try {
+            hashTable.print(new PrintStream("data/restored.txt", "UTF-8"));
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
