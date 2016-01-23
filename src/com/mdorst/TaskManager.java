34c353ab01a6c456f4552ddcccfa1baaf1f1ecf6
Michael Dorst <code@mikefdorst.com>
2016-01-22 20:46:19 -0800

    Add TaskManager

---
+++ src/com/mdorst/TaskManager.java
@@ -0,0 +1,15 @@
+package com.mdorst;
+
+/**
+ * Michael Dorst
+ * CISP 430
+ * Assignment 1 - Hashing
+ */
+public class TaskManager {
+    // input from datain.txt
+    // output to dataout.txt
+    // restore from dataout.txt
+    // display contents
+    // search
+    // compute hash efficiency statistics
+}
