ab3698d6577e0b05a2b65a31712158ca1528f256
Michael Dorst <code@mikefdorst.com>
2016-02-03 00:55:55 -0800

    Remove menu based system

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -15,51 +15,14 @@ public class Main {
 
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
-        printMenu();
-        try {
-            while (true) {
-                int ch = System.in.read();
-                if (ch == 'x' || ch == 'X') {
-                    break;
-                }
-                if (ch == '1') {
-                    taskManager.read();
-                }
-                if (ch == '2') {
-                    taskManager.write();
-                }
-                if (ch == '3') {
-                    taskManager.display();
-                }
-                if (ch == '4') {
-                    taskManager.restore();
-                }
-                if (ch == '5') {
-                    out.print("Query: ");
-                    Scanner input = new Scanner(System.in);
-                    input.nextLine();
-                    out.println(taskManager.search(input.nextLine()));
-                }
-                if (ch == '6') {
-                    out.println("Average Collision Chain Length:" + taskManager.computeAverageChainLength());
-                }
-                if (ch == '?') {
-                    printMenu();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void printMenu() {
-        out.println("1 Read from datain.txt");
-        out.println("2 Write to report.txt");
-        out.println("3 Disply data report");
-        out.println("4 Restore data from report.txt");
-        out.println("5 Search");
-        out.println("6 Compute efficiency statistics");
-        out.println("? Display this menu");
-        out.println("X Exit");
+        taskManager.read();
+        taskManager.write();
+        taskManager.restore();
+        // TODO Search
+//        out.print("Query: ");
+//        Scanner input = new Scanner(System.in);
+//        input.nextLine();
+//        out.println(taskManager.search(input.nextLine()));
+        out.println("Average Collision Chain Length:" + taskManager.computeAverageChainLength());
     }
 }
